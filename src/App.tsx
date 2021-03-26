d816f01a1d81657e52f5db8a1c5b7c95b03d2172
Krzysztof Kowalczyk <kowalczykkrzysztof893@gmail.com>
2021-03-26 03:18:56 +0100

    Added font awesome library

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faReact } from '@fortawesome/free-brands-svg-icons';
+import { faLanguage } from '@fortawesome/free-solid-svg-icons';
 
 //Components
 import Navbar from './components/navbars/Navbar';
 import Footer from './components/footer/Footer';
 import AboutMe from './components/aboutme/AboutMe';
+
+// Adding icons to library makes it so I don't need to import icons in every file
+library.add(faLanguage, faReact);
 
 const App = (): JSX.Element => {
   return (
