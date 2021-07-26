2c0135bd6f4864a727b91af64c46f2b0b302c850
Krzysztof Kowalczyk <kowalczykkrzysztof893@gmail.com>
2021-07-26 14:52:57 +0200

    Workbox changes

--- workbox-config.js
+++ workbox-config.js
@@ -1,11 +1,6 @@
 module.exports = {
-	globDirectory: 'src/',
-	globPatterns: [
-		'**/*.{tsx,ts}'
-	],
-	ignoreURLParametersMatching: [
-		/^utm_/,
-		/^fbclid$/
-	],
-	swDest: 'public/sw.js'
+  globDirectory: 'src/',
+  globPatterns: ['**/*.{tsx,ts,png}'],
+  ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
+  swDest: 'public/sw.js',
 };
