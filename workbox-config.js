68f848f7382cc457ee0da0a159033e8c5c7ca7f3
Krzysztof Kowalczyk <kowalczykkrzysztof893@gmail.com>
2021-07-27 22:29:27 +0200

    Image caching

--- workbox-config.js
+++ workbox-config.js
@@ -3,4 +3,16 @@ module.exports = {
   globPatterns: ['**/*.{tsx,ts}'],
   ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
   swDest: 'public/sw.js',
+  runtimeCaching: [
+    {
+      urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+
+      // Apply a cache-first strategy.
+      handler: 'CacheFirst',
+      options: {
+        // Use a custom cache name.
+        cacheName: 'images',
+      },
+    },
+  ],
 };
