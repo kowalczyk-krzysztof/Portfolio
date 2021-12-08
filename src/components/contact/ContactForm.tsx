f803d60f38cdb0d2acbe5cc9dba97826475e5b35
Krzysztof Kowalczyk <krzysztof.kowalczyk@bpower2.com>
2021-12-08 05:02:51 +0100

    ContactForm DRYing

--- src/components/contact/ContactForm.tsx
+++ src/components/contact/ContactForm.tsx
@@ -21,36 +21,46 @@ import {
   StyledButtonContainer,
 } from './contact-styling';
 
-// Warning visibility
 export enum WarningVisibility {
   SHOW = 1,
   HIDDEN = 0,
 }
-// Field border
+
+export enum EmailSendingStyle {
+  LOADING = 'LOADING',
+  SUCCESS = 'SUCCESS',
+  FAILURE = 'FAILURE',
+}
+
 enum FieldBorderStyle {
   DEFAULT = '#86B7FE',
   SUCCESS = '#198754',
   FAILURE = '#DC3545',
 }
-// Field box shadow
+
 enum FieldShadowStyle {
   DEFAULT = '#C2DBFE',
   SUCCESS = '#C5E1D4',
   FAILURE = '#F6CCD0',
 }
 
-// Input fields
 enum FieldNames {
   NAME = 'name',
   EMAIL = 'email',
   MESSAGE = 'message',
 }
-// Button styles
-export enum EmailSendingStyle {
-  LOADING = 'LOADING',
-  SUCCESS = 'SUCCESS',
-  FAILURE = 'FAILURE',
-}
+
+type WarningState = {
+  [key in FieldNames]: WarningVisibility;
+};
+
+type BorderState = {
+  [key in FieldNames]: FieldBorderStyle;
+};
+
+type ShadowState = {
+  [key in FieldNames]: FieldShadowStyle;
+};
 
 // Emailjs settings
 const serviceID: string = `${process.env.REACT_APP_EMAILJS_SERVICEID}`;
@@ -58,8 +68,6 @@ const templateID: string = `${process.env.REACT_APP_EMAILJS_TEMPLATEID}`;
 const userID: string = `${process.env.REACT_APP_EMAILJS_USERID}`;
 
 export const ContactForm: FC = (): JSX.Element => {
-  /* FORM SETTINGS */
-
   // Form id
   const formId: string = 'contact';
   // Text area max rows
@@ -67,7 +75,6 @@ export const ContactForm: FC = (): JSX.Element => {
   // Max textarea character length
   const messageMaxLength: number = 256;
 
-  /* LOCALIZATION */
   const localization: Locale = useSelector(localizationSelector);
   const {
     contactNameField,
@@ -80,170 +87,120 @@ export const ContactForm: FC = (): JSX.Element => {
     buttonSend,
   } = localization;
 
-  /*                                  STATES                                        */
+  const [warnings, setWarnings] = useState<WarningState>({
+    [FieldNames.NAME]: WarningVisibility.HIDDEN,
+    [FieldNames.EMAIL]: WarningVisibility.HIDDEN,
+    [FieldNames.MESSAGE]: WarningVisibility.HIDDEN,
+  });
 
-  /* 
-      NAME FIELD  
-  */
+  const [borders, setBorders] = useState<BorderState>({
+    [FieldNames.NAME]: FieldBorderStyle.DEFAULT,
+    [FieldNames.EMAIL]: FieldBorderStyle.DEFAULT,
+    [FieldNames.MESSAGE]: FieldBorderStyle.DEFAULT,
+  });
 
-  // State of input
-  const [name, setName] = useState<string>(``);
-  // Check whether to display warning or not
-  const [nameWarning, setNameWarning] = useState<WarningVisibility>(
-    WarningVisibility.HIDDEN
-  );
-  // Setting border for name field
-  const [nameBorder, setNameBorder] = useState<FieldBorderStyle>(
-    FieldBorderStyle.DEFAULT
-  );
-  // Setting box shadow for name field
-  const [nameShadow, setNameShadow] = useState<FieldShadowStyle>(
-    FieldShadowStyle.DEFAULT
-  );
+  const [shadows, setShadows] = useState<ShadowState>({
+    [FieldNames.NAME]: FieldShadowStyle.DEFAULT,
+    [FieldNames.EMAIL]: FieldShadowStyle.DEFAULT,
+    [FieldNames.MESSAGE]: FieldShadowStyle.DEFAULT,
+  });
 
-  /* 
-      EMAIL FIELD  
-  */
-
-  // State of input
-  const [email, setEmail] = useState<string>(``);
-  // Check if email is empty
-  const [emailWarning, setEmailWarning] = useState<WarningVisibility>(
-    WarningVisibility.HIDDEN
-  );
-  // Setting border for email field
-  const [emailBorder, setEmailBorder] = useState<FieldBorderStyle>(
-    FieldBorderStyle.DEFAULT
-  );
-  // Setting box shadow for email field
-  const [emailShadow, setEmailShadow] = useState<FieldShadowStyle>(
-    FieldShadowStyle.DEFAULT
-  );
-
-  /* 
-      MESSAGE FIELD  
-  */
-
-  // State of input
+  const [name, setName] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
   const [message, setMessage] = useState<string>('');
-  // Check if message is empty
-  const [messageWarning, setMessageWarning] = useState<WarningVisibility>(
-    WarningVisibility.HIDDEN
-  );
-  // Setting border for message field
-  const [messageBorder, setMessageBorder] = useState<FieldBorderStyle>(
-    FieldBorderStyle.DEFAULT
-  );
-  // Setting box shadow for message field
-  const [messageShadow, setMessageShadow] = useState<FieldShadowStyle>(
-    FieldShadowStyle.DEFAULT
-  );
+
   // Display characters left in text area
   const [charactersLeft, setcharactersLeft] =
     useState<number>(messageMaxLength);
-
   // Email was sent
   const [isEmailSent, setIsEmailSent] = useState<EmailSendingStyle | string>(
     ''
   );
-
-  /* UTILS */
-
   // Resetting form fields
   const resetForm = (): void => {
     setName('');
     setEmail('');
     setMessage('');
     setcharactersLeft(messageMaxLength);
-    setNameBorder(FieldBorderStyle.DEFAULT);
-    setNameShadow(FieldShadowStyle.DEFAULT);
-    setEmailBorder(FieldBorderStyle.DEFAULT);
-    setEmailShadow(FieldShadowStyle.DEFAULT);
-    setMessageBorder(FieldBorderStyle.DEFAULT);
-    setMessageShadow(FieldShadowStyle.DEFAULT);
+    setBorders({
+      [FieldNames.NAME]: FieldBorderStyle.DEFAULT,
+      [FieldNames.EMAIL]: FieldBorderStyle.DEFAULT,
+      [FieldNames.MESSAGE]: FieldBorderStyle.DEFAULT,
+    });
+    setShadows({
+      [FieldNames.NAME]: FieldShadowStyle.DEFAULT,
+      [FieldNames.EMAIL]: FieldShadowStyle.DEFAULT,
+      [FieldNames.MESSAGE]: FieldShadowStyle.DEFAULT,
+    });
   };
-  // Resetting warnings
+
   const resetWarnings = (): void => {
-    setNameWarning(WarningVisibility.HIDDEN);
-    setEmailWarning(WarningVisibility.HIDDEN);
-    setMessageWarning(WarningVisibility.HIDDEN);
+    setWarnings({
+      [FieldNames.NAME]: WarningVisibility.HIDDEN,
+      [FieldNames.EMAIL]: WarningVisibility.HIDDEN,
+      [FieldNames.MESSAGE]: WarningVisibility.HIDDEN,
+    });
   };
 
-  /* FORM METHODS */
+  const valueCheck = (fieldName: FieldNames, value: string | boolean): void => {
+    let warningValue: WarningVisibility = WarningVisibility.HIDDEN;
+    let borderValue: FieldBorderStyle = FieldBorderStyle.SUCCESS;
+    let shadowsValue: FieldShadowStyle = FieldShadowStyle.SUCCESS;
+    if (!value) {
+      warningValue = WarningVisibility.SHOW;
+      borderValue = FieldBorderStyle.FAILURE;
+      shadowsValue = FieldShadowStyle.FAILURE;
+    }
+    setWarnings({ ...warnings, [fieldName]: warningValue });
+    setBorders({ ...borders, [fieldName]: borderValue });
+    setShadows({ ...shadows, [fieldName]: shadowsValue });
+  };
 
-  // On change
   const onChange = (
     e: FormEvent<HTMLInputElement | HTMLTextAreaElement>
   ): void => {
     const targetValue: string = e.currentTarget.value;
     const targetName: string = e.currentTarget.name;
-    // Assigning values to respective states
+
     if (targetName === FieldNames.NAME) {
       setName(targetValue);
-      if (!targetValue) {
-        setNameWarning(WarningVisibility.SHOW);
-        setNameBorder(FieldBorderStyle.FAILURE);
-        setNameShadow(FieldShadowStyle.FAILURE);
-      } else {
-        setNameWarning(WarningVisibility.HIDDEN);
-        setNameBorder(FieldBorderStyle.SUCCESS);
-        setNameShadow(FieldShadowStyle.SUCCESS);
-      }
+      valueCheck(targetName, targetValue);
     }
     if (targetName === FieldNames.EMAIL) {
       setEmail(targetValue);
-      if (!regexCheck(targetValue)) {
-        setEmailWarning(WarningVisibility.SHOW);
-        setEmailBorder(FieldBorderStyle.FAILURE);
-        setEmailShadow(FieldShadowStyle.FAILURE);
-      } else {
-        setEmailWarning(WarningVisibility.HIDDEN);
-        setEmailBorder(FieldBorderStyle.SUCCESS);
-        setEmailShadow(FieldShadowStyle.SUCCESS);
-      }
+      valueCheck(targetName, regexCheck(targetValue));
     }
     if (targetName === FieldNames.MESSAGE) {
-      // IMPORTANT: Make sure im doing the math for characters left on e.currentTarget.value, NOT state
       setcharactersLeft(messageMaxLength - targetValue.length);
       setMessage(targetValue);
-      if (!targetValue) {
-        setMessageWarning(WarningVisibility.SHOW);
-        setMessageBorder(FieldBorderStyle.FAILURE);
-        setMessageShadow(FieldShadowStyle.FAILURE);
-      } else {
-        setMessageWarning(WarningVisibility.HIDDEN);
-        setMessageBorder(FieldBorderStyle.SUCCESS);
-        setMessageShadow(FieldShadowStyle.SUCCESS);
-      }
+      valueCheck(targetName, targetValue);
     }
   };
 
-  // On submit
   const onSubmit = async (e: FormEvent<HTMLFormElement>): Promise<void> => {
     e.preventDefault();
-    /* VALIDATING FIELDS 
-      name, email and message are states of fields (so e.currentTarget.value from onChange)
-    */
+    const emailValidation: boolean = regexCheck(email);
 
-    // Validating name field
-    if (name) setName(name);
-    else setNameWarning(WarningVisibility.SHOW);
+    if (!name)
+      setWarnings((warnings) => ({
+        ...warnings,
+        [FieldNames.NAME]: WarningVisibility.SHOW,
+      }));
 
-    // Valdiating email field
-    const emailValidation = regexCheck(email);
-    if (emailValidation) setEmail(email);
-    else setEmailWarning(WarningVisibility.SHOW);
+    if (!emailValidation)
+      setWarnings((warnings) => ({
+        ...warnings,
+        [FieldNames.EMAIL]: WarningVisibility.SHOW,
+      }));
 
-    // Validating message field
-    if (message) setMessage(message);
-    else setMessageWarning(WarningVisibility.SHOW);
+    if (!message)
+      setWarnings((warnings) => ({
+        ...warnings,
+        [FieldNames.MESSAGE]: WarningVisibility.SHOW,
+      }));
 
-    // If all fields are valid then change the button style to loading, reset all fields, try to send email then let user know if email was sent by changing the button style and displaying a notificiations
     if (name && emailValidation && message) {
-      // Set email notificaiton to loading for when the POST request is processing
-
       setIsEmailSent(EmailSendingStyle.LOADING);
-      // Checking if email was sent
       const res: EmailJSResponseStatus = await emailjs.send(
         serviceID,
         templateID,
@@ -252,7 +209,6 @@ export const ContactForm: FC = (): JSX.Element => {
       );
       if (res?.status === 200) {
         setIsEmailSent(EmailSendingStyle.SUCCESS);
-        // Resetting form state and hiding notifications
         resetForm();
         resetWarnings();
       } else {
@@ -266,8 +222,8 @@ export const ContactForm: FC = (): JSX.Element => {
       <StyledFormElement>
         <StyledLabel htmlFor={FieldNames.NAME}>{contactNameField}</StyledLabel>
         <StyledInput
-          shadow={nameShadow}
-          border={nameBorder}
+          shadow={shadows[FieldNames.NAME]}
+          border={borders[FieldNames.NAME]}
           type="text"
           onChange={onChange}
           name={FieldNames.NAME}
@@ -275,7 +231,7 @@ export const ContactForm: FC = (): JSX.Element => {
           placeholder={contactNameField}
           data-testid={'contactformname'}
         ></StyledInput>
-        <StyledNotificationWarning visibility={nameWarning}>
+        <StyledNotificationWarning visibility={warnings[FieldNames.NAME]}>
           {enterName}
         </StyledNotificationWarning>
       </StyledFormElement>
@@ -284,8 +240,8 @@ export const ContactForm: FC = (): JSX.Element => {
           {contactEmailField}
         </StyledLabel>
         <StyledInput
-          shadow={emailShadow}
-          border={emailBorder}
+          shadow={shadows[FieldNames.EMAIL]}
+          border={borders[FieldNames.EMAIL]}
           type="email"
           onChange={onChange}
           name={FieldNames.EMAIL}
@@ -293,7 +249,7 @@ export const ContactForm: FC = (): JSX.Element => {
           placeholder={contactEmailField}
           data-testid={'contactformemail'}
         ></StyledInput>
-        <StyledNotificationWarning visibility={emailWarning}>
+        <StyledNotificationWarning visibility={warnings[FieldNames.EMAIL]}>
           {invalidEmail}
         </StyledNotificationWarning>
       </StyledFormElement>
@@ -302,8 +258,8 @@ export const ContactForm: FC = (): JSX.Element => {
           {contactMessageField}
         </StyledLabel>
         <StyledTextArea
-          shadow={messageShadow}
-          border={messageBorder}
+          shadow={shadows[FieldNames.MESSAGE]}
+          border={borders[FieldNames.MESSAGE]}
           rows={maxRows}
           maxLength={messageMaxLength}
           onChange={onChange}
@@ -312,7 +268,7 @@ export const ContactForm: FC = (): JSX.Element => {
           placeholder={contactMessageField}
           data-testid={'contactformemessage'}
         ></StyledTextArea>
-        <StyledNotificationWarning visibility={messageWarning}>
+        <StyledNotificationWarning visibility={warnings[FieldNames.MESSAGE]}>
           {enterMessage}
         </StyledNotificationWarning>
         <StyledCharactersLeft>
